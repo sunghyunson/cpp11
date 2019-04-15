22c4ab98c03e518d0beeba7c900a3c33ebeab9ec
son <sh.son@lgepartner.com>
2019-04-15 18:31:16 +0900

    init

---
+++ .vscode/build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+makeFileName="Makefile"
+PWD=$1
+cd "$PWD"
+# depth=0
+root="/"
+found="true"
+
+while [[ ! -f "$makeFileName" ]]
+do
+	# echo "PWD : $PWD"
+	cd ..
+	# depth=$depth+1
+	# if (( $depth > 10 ))
+	# then
+	# 	found="false"
+	# 	break
+	# fi
+	if [[ "$PWD" = "$root" ]]
+	then
+		found="false"
+		break
+	fi
+done
+
+if [[ "$found" = "false" ]]
+then
+	echo "ERROR --> no make file"
+	exit 1
+else
+	make $2
+fi
+
+#echo "Compiled Dir is : $PWD"
---
+++ trait/trait.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+#include <type_traits>
+
+using namespace std;
+
+void print_separator(string str) { cout << "------" << str.c_str() << "------\n"; }
+
+void trait_is_void() {
+	cout << boolalpha;
+	cout << is_void<void>::value << '\n';
+	cout << is_void<int>::value << '\n';
+	cout << noboolalpha;
+}
+
+template <class T>
+T f(T i) {
+	static_assert(is_integral<T>::value, "Integral required.");
+	cout << i << endl;
+	return 1;
+}
+
+void trait_is_nullptr() {
+	cout << is_null_pointer<decltype(nullptr)>() << ' ' << is_null_pointer<int*>::value << '\n'
+		 << is_pointer<decltype(nullptr)>::value << ' ' << is_pointer<int*>::value << '\n';
+}
+
+void trait_is_integral() {
+	int a = 3;
+	cout << is_integral<float>::value << '\n';
+	cout << is_integral<bool>::value << '\n';
+	cout << is_integral<decltype(a)>::value << '\n';
+	// cout << is_integral<a>::value << '\n'; // error
+}
+
+int* func(int* a, int* b) {
+	cout << *a << endl;
+	cout << *b << endl;
+	return a;
+}
+
+class dummy {
+public:
+	dummy();
+
+private:
+	void ppp();
+};
+
+void trait_is_same() {
+	cout << boolalpha;
+	cout << "is_same<int, int32_t>() " << is_same<int, int32_t>() << '\n';
+	cout << "is_same<int, int64_t>() " << is_same<int, int64_t>() << '\n';
+}
+
+int main() {
+	cout << "trait " << endl;
+	print_separator("is_void");
+	trait_is_void();
+	print_separator("is_nullptr");
+	trait_is_nullptr();
+	print_separator("is_integral");
+	trait_is_integral();
+	print_separator("is_same");
+	trait_is_same();
+	return 0;
+}
